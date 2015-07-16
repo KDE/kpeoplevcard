118db4985c3ec9c7e03be844876f747b7a52c6ff
Martin Klapetek <mklapetek@kde.org>
2015-07-16 18:45:56 +0200

    Add copyright

--- src/kpeoplevcard.cpp
+++ src/kpeoplevcard.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2015 Aleix Pol Gonzalez <aleixpol@kde.org>
+    Copyright (C) 2015 Martin Klapetek <mklapetek@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
